3cd5d1df93dd61636fb062fd853de74db286d357
eunji2971 <panda.ohgiraffers@gmail.com>
2025-03-14 11:15:22 +0900

    #3 hello 출력 추가

--- src/main/java/com/ohgiraffers/testt/TesttApplication.java
+++ src/main/java/com/ohgiraffers/testt/TesttApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TesttApplication {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello~");
         SpringApplication.run(TesttApplication.class, args);
     }
 
